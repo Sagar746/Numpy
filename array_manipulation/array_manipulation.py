e35c7dac30f15dcbbda5a5e022246eaa2671fe50
Sagar746 <tiwarisagar389@gmail.com>
2024-08-25 22:02:12 +0300

    add detail append

--- array_manipulation/array_manipulation.py
+++ array_manipulation/array_manipulation.py
@@ -5,3 +5,28 @@ arr = np.array([1,2,3,4,5])
 # append at the end of the array
 ap = np.append(arr, [6,7,8,9])
 print(ap)
+
+
+# -----------------------------------------------------------
+
+arr = np.arange(1,13).reshape(4,3)
+
+print(f'array to be appended columnwise')
+col = np.arange(5,11).reshape(1,6)
+print(col)
+
+print(f'array after appending column-wise')
+arr_col = np.append(arr,col, axis=0)
+print(arr_col) 
+
+
+# -------------- SWAPPING THE COLUMN OF ARRAY --------------
+
+print(arr)
+print('\n')    #new line
+
+arr[:, [0,2]] = arr[:, [2,0]]
+print(arr)
+
+
+# -------------------------------------------------------------
