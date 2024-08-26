6aa5961107d7b1c19feaf6a466f83bba7ec44237
Sagar746 <tiwarisagar389@gmail.com>
2024-08-26 17:34:17 +0300

    enumeration numpy array

--- array_manipulation/concat.py
+++ array_manipulation/concat.py
@@ -10,3 +10,14 @@ arr2 = np.array([3,4])
 
 array_new = np.concatenate((arr1, arr2))
 print(array_new)
+
+
+# ---------------------------------------------
+
+    # ENUMERATE NUMPY ARRAY
+
+arr = np.arange(5)
+arr1 = np.arange(10).reshape(2,5)
+
+for a,b in np.nditer([arr, arr1]):
+    print(f'{a} : {b}')
