36aa906f48c6a1415594d25741fb988aa91a291b
Sagar746 <tiwarisagar389@gmail.com>
2024-08-28 02:38:17 +0300

    add functools for more than two arrays

--- array_manipulation/union.py
+++ array_manipulation/union.py
@@ -7,3 +7,23 @@ arr2 = np.array([5,6,7,8,9])
 
 result = np.union1d(arr1, arr2)
 print(result)
+
+
+
+
+# ------------------------------------
+
+'''
+If we want to find union of more than two arrays, then we can 
+find that by using functools.reduce function
+'''
+
+from functools import reduce
+
+arr1 = np.array([[1,2,3], [4,5,6],[10,11,12]])
+
+arr2 = np.array([[0,5,1], [7,8,9],[11,13,14]])
+
+
+result = reduce(np.union1d, (arr1,arr2))
+print(result)
