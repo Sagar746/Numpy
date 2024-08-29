d1b83ac9f391f83abc268d33363d855f96a93ab1
Sagar746 <tiwarisagar389@gmail.com>
2024-08-29 18:59:31 +0300

    basic matrix code

---
+++ matrix_multiplication/matrix.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+x = np.array([[1,2], [4,5]])
+y = np.array([[7,8], [9,10]])
+
+add1 = np.add(x,y)
+print(add1)
+
+sub = np.subtract(x,y)
+print(sub)
+
+div = np.divide(x,y)
+print(div)
+
+mul = np.multiply(x,y)
+print(mul)
+
+dot = np.dot(x,y)
+print(dot)
+
+# tranpose of matrix
+
+print(x.T)
+
