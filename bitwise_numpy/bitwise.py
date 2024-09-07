ece373a9dc638463fcd77001b38c9cd76bcf86ae
Sagar746 <tiwarisagar389@gmail.com>
2024-09-08 02:18:38 +0300

    add bitwise operator

---
+++ bitwise_numpy/bitwise.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+# numpy bitwise operator
+
+
+num1 = 10
+num2 = 11
+
+result = np.bitwise_and(num1, num2)
+print(result)
+
+
+
+result = np.bitwise_or(num1, num2)
+print(result)
