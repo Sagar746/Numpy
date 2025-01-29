4af99cb43ac32ef9c627b019a4e7a1c977d8c734
Sagar Tiwari <51308206+Sagar746@users.noreply.github.com>
2025-01-29 23:50:21 +0545

    Update linspace.py

--- create_numpy/linspace.py
+++ create_numpy/linspace.py
@@ -4,9 +4,9 @@ import numpy as np
     np.linspace(start, stop, num)
 '''
 
-x = np.linspace(2,10, num=5)
+x, step = np.linspace(2,10, num=5, retstep=True)
 print(x, '\n')
-
+print(step)
 # -----------------------------
 
 y = np.eye(2,4) 
